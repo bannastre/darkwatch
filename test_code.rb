e6974c46cabbc7d7fbb74a081345be9290bc4849
Chris Harrop <chris@jigsaw.xyz>
2018-05-22 11:30:01 +0100

    add control.rubyblock end to colour theme

--- test_code.rb
+++ test_code.rb
@@ -35,3 +35,20 @@ class Grid
     end[0].delete("\n")
   end
 end
+
+class App < Sinatra::Base
+  get "/sessions/new" do
+    erb :"sessions/new"
+  end
+
+  post "/sessions" do
+    user = Login.new(User).user(params[:email], params[:password])
+    session[:user_id] = user.id if user
+    redirect "/"
+  end
+
+  delete "/sessions" do
+    session.delete(:user_id)
+    redirect "/"
+  end
+end
